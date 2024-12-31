109eca8b7d8dc89eb5de57421481e9ddf82b9a02
mie035 <hello@michitoshiyano.com>
2024-12-31 13:50:32 +0900

    add vitest

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+/// <reference types="vitest/config" />
+
+// Configure Vitest (https://vitest.dev/config/)
+
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  test: {
+  },
+})
