a6b3ed9a9fcf0cf4b83c1493c0e0271925b3278c
mie035 <hello@michitoshiyano.com>
2024-12-31 14:27:45 +0900

    add deadline feature

--- src/domain/task.ts
+++ src/domain/task.ts
@@ -8,6 +8,8 @@ export class Task {
     private id: string;
     private title: string;
     private description: string;    
+    private deadline?: Date;
+    private postponedCount: number = 0;
     private status: Status;
 
     constructor(id: string, title: string, description: string) {
@@ -29,12 +31,34 @@ export class Task {
         return this.description;
     }
 
+    public getDeadline(): Date | undefined {
+        return this.deadline;;
+    }
+
     public getStatus(): Status {
         return this.status;;
     }
 
     public updateTitle(title: string): void {
         this.title = title;
+    }
+
+    public setDeadline(deadline: Date): void {
+        if(this.deadline !== undefined) {
+            throw new Error('Cannot set deadline for a task that already has a deadline');
+        }
+        this.deadline = deadline;
+    }
+
+    public postpone(): void {
+        if(this.deadline === undefined) {
+            throw new Error('Cannot postpone a task without a deadline');
+        }
+        if(this.postponedCount >= 3) {
+            throw new Error('Cannot postpone a task more than 3 times');
+        }
+        this.deadline.setDate(this.deadline.getDate() + 1);
+        this.postponedCount++;
     }
 
     public updateDescription(description: string): void {
--- test/task.test.ts
+++ test/task.test.ts
@@ -3,9 +3,11 @@ import { Status, Task } from '../src/domain/task.js';
 
 describe('task suite', () => {
   const task = new Task('1', 'Task 1', 'Description of Task 1');
+
     it('初期値が設定されていること', () => {
     assert.equal(task.getTitle(), "Task 1")
     assert.equal(task.getDescription(), "Description of Task 1")
+    assert.equal(task.getDeadline(), undefined)
     assert.equal(task.getStatus(), Status.NOT_STARTED)
   })
     it('タイトルが更新されること', () => {
@@ -19,6 +21,22 @@ describe('task suite', () => {
     it('ステータスが更新されること', () => {
         task.updateStatus(Status.IN_PROGRESS)
         assert.equal(task.getStatus(), Status.IN_PROGRESS)
+    })
+    const selectedDate = new Date("2024-12-31"); 
+    it('期日が設定されること', () => {  
+        task.setDeadline(selectedDate)
+        assert.equal(task.getDeadline(), selectedDate)
+    })
+    it('期日を3回延長できること', () => {
+        task.postpone();
+        task.postpone();
+        task.postpone();
+        assert.equal(task.getDeadline()!.toISOString(), new Date("2025-01-03").toISOString())
+    })
+    it('期日を4回延長できないこと', () => {
+        assert.throws(() => {
+            task.postpone();
+        })
     })
     it('完了タスクのステータスは更新できないこと', () => {
         task.updateStatus(Status.COMPLETED)
